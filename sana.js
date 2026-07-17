e25fb6b08dafd0b4b2023fab07ca26d73229b7ec
abhacci <85144667+abhacci@users.noreply.github.com>
2026-07-17 14:56:11 +0300

    Update sana.js

--- sana.js
+++ sana.js
@@ -1,1 +1,31 @@
- 
+ // =========================
+// Sana Engine v1
+// =========================
+
+class Sana {
+
+    constructor() {
+        this.name = "سنا";
+    }
+
+    say(text) {
+
+        const bubble = document.getElementById("speechBubble");
+
+        if (!bubble) return;
+
+        bubble.innerHTML = text;
+
+        bubble.classList.add("show");
+
+        setTimeout(() => {
+
+            bubble.classList.remove("show");
+
+        }, 4000);
+
+    }
+
+}
+
+const sana = new Sana();
